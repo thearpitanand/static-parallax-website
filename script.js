5ce09c371dc7296caeba76de7d19d41485a2a8be
Arpit Anand <arpitssanand@gmail.com>
2021-05-21 12:55:36 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".rock", 3, { y: -300 })
+  .to(".girl", 3, { y: -200 }, "=-3")
+  .fromTo(".bg1", { y: -50 }, { y: 0, duration:3 }, "=-3")
+  .to(".content", 3, { top: "0%" }, "=-3")
+  .fromTo(".content-images",{opacity:0},{opacity:1,duration:1});
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "100%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
